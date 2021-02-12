2e4ac213f4b870eb569866a8bc89c46397b22637
Ivan Jamandilovski <jamandilovski@gmail.com>
2021-02-12 21:30:06 +0100

    updated recursion in recursion, in one function

--- 03 Advanced Javascript/Recursions/4 Table using Recursions/script.js
+++ 03 Advanced Javascript/Recursions/4 Table using Recursions/script.js
@@ -1,43 +1,39 @@
 //script.js
 //Write a function that will print table using Javascript arrow functions, 
-//the table should have x columns and y rows. 
+//the table should have x rows and y columns. 
 //The values for x and y are inserted by the user.
 
 let rowsInput = document.getElementById('rows');
 let columnsInput = document.getElementById('columns');
 let table = document.getElementById('table');
 let submit = document.getElementById('submit');
+
 let trArray = ``;
-columnCounter = 0;
+let columnCounter = 0;
 
-let columnDraw = (columns) => {
-    if (columns === 0) {
-        return 0;
-    }
-    else {
-        columnDraw(columns - 1)
-        columnCounter++
-        trArray += `<td style="background-color: rgb(255,${(Math.random() * 255)},0);">Column ${columnCounter}</td>`
-    }
-    return trArray;
-}
-
-let tableDraw = (rows) => {
+let tableDraw = (rows, columns) => {
     if (rows === 0) {
         return 0;
     }
+    if (columns === 0) {
+        tableDraw(rows - 1, columns);
+        columnCounter++
+        trArray += `<td style="background-color: rgb(255,${(Math.random() * 255)},0);">Column ${columnCounter}</td>`
+        return 0;
+    }
     else {
-        tableDraw(rows - 1);
-        return table.innerHTML += `<tr> ${trArray}</tr>`
+        tableDraw(rows, columns - 1);
+        table.innerHTML += `<tr> ${trArray}</tr>`
+        return 0;
     }
 }
 
-
 submit.addEventListener('click', () => {
-    columnDraw(columnsInput.value)
 
     table.innerHTML = '';
-    tableDraw(rowsInput.value);
+    tableDraw(columnsInput.value, rowsInput.value,);
     rowsInput.value = ``
     columnsInput.value = ``
+    trArray = ``;
+    columnCounter = 0;
 })
