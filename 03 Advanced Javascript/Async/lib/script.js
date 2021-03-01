b49c5d7b60b9bc755aa77ef48b17459ff416d384
Ivan Jamandilovski <jamandilovski@gmail.com>
2021-03-01 01:21:34 +0100

    homework done

--- 03 Advanced Javascript/Async/lib/script.js
+++ 03 Advanced Javascript/Async/lib/script.js
@@ -20,7 +20,8 @@ let page = {
         if (this.blocValue.value.length > 0) {
             page.blocDiv.innerHTML = ``;
             apiData.bloc = blocValue.value;
-            apiData.getDataBloc();
+            //apiData.getDataBloc();
+            apiData.getDataBlocWithAjax();
             this.blocValue.value = ``;
         }
     }),    
@@ -44,9 +45,9 @@ let apiData = {
     iso: `mkd`,
     apiUrl: `https://restcountries.eu/rest/v2/`,
 
-    //FETCH FOR BLOC
+    //FETCH FOR BLOC WITH ASYNC/AWAIT SYNTAX
     getDataBloc: async function getBlocCode() {
-        let data = await fetch(`${apiData.apiUrl}regionalbloc/${apiData.bloc}`);
+        let data = await fetch(`${this.apiUrl}regionalbloc/${this.bloc}`);
         let bloc = await data.json();
 
         if (bloc.length > 0) {
@@ -59,9 +60,30 @@ let apiData = {
         }
     },
 
+    //SAME AS ABOVE ONLY WITH AJAX SYNTAX
+    getDataBlocWithAjax: function() {
+        $.ajax({
+            url: `${this.apiUrl}regionalbloc/${this.bloc}`,
+            success: function(bloc) {                     
+                if (bloc.length > 0) {
+                    bloc.map(country =>
+                        //THE FIRST TASK PARAMETERS with ajax
+                        country.altSpellings.length >= 1 && country.altSpellings.length <= 4
+                            ? page.blocDiv.innerHTML += `${country.altSpellings.length} alternative spelling names for <b>${country.name}</b>: ${country.altSpellings}<br>`
+                            : null
+                    )
+                }
+            },
+            error: function(error) {
+                console.warn("The request has failed")
+                console.error(error.responseText)
+            }
+        })
+    },
+
     //FETCH FOR ISO
     getDataISO: async function getISOCode() {
-        let data = await fetch(`${apiData.apiUrl}alpha/${apiData.iso}`);
+        let data = await fetch(`${this.apiUrl}alpha/${this.iso}`);
         let iso = await data.json();
         getSameCurrencyCountries();
         
@@ -81,3 +103,4 @@ let apiData = {
 
 apiData.getDataBloc();
 apiData.getDataISO();
+
