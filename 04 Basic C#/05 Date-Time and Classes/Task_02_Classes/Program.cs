25c6027859648366dca0ab0e9dfa9b67fde52628
Ivan Jamandilovski <jamandilovski@gmail.com>
2021-04-09 14:41:47 +0200

    added classes homework for racing cars and dateTime

--- 04 Basic C#/05 Date-Time and Classes/Task_02_Classes/Program.cs
+++ 04 Basic C#/05 Date-Time and Classes/Task_02_Classes/Program.cs
@@ -69,9 +69,9 @@ namespace Task_02_Classes
             Console.WriteLine("Select driver from the list: ");
             for (byte i = 0; i < drivers.Length; i++)
             {
-                Console.WriteLine($"{i+1}. {drivers[i].Name}");
+                Console.WriteLine($"{i + 1}. {drivers[i].Name}");
             }
-            Console.Write("INPUT THE NUMBER OF THE DRIVER: ");
+            Console.Write("INPUT THE NUMBER OF THE FRIST CAR DRIVER: ");
             byte numberOfDriver = 0;
             bool isValidDriverNumber = byte.TryParse(Console.ReadLine(), out numberOfDriver);
 
@@ -83,9 +83,83 @@ namespace Task_02_Classes
             {
                 Console.WriteLine($"{i + 1}. {cars[i].Model}");
             }
-            Console.Write("INPUT THE NUMBER OF THE CAR: ");
+            Console.Write("INPUT THE NUMBER OF THE FIRST CAR: ");
             byte numberOfCar = 0;
             bool isValidCarNumber = byte.TryParse(Console.ReadLine(), out numberOfCar);
+
+
+            Console.WriteLine("------------------------------");
+
+            Console.Write("INPUT THE NUMBER OF THE SECOND CAR DRIVER: ");
+            Console.WriteLine("Select driver from the list: ");
+            for (byte i = 0; i < drivers.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {drivers[i].Name}");
+            }
+            byte numberOfDriver2 = 0;
+            bool isValidDriverNumber2 = byte.TryParse(Console.ReadLine(), out numberOfDriver2);
+            
+            while (true)
+            {
+                if (numberOfDriver == numberOfDriver2)
+                {
+                    Console.WriteLine("That driver is already racing, pleace choose another driver ");
+                    isValidDriverNumber2 = byte.TryParse(Console.ReadLine(), out numberOfDriver2);
+                }
+                else break;
+            }
+            Console.WriteLine("------------------------------");
+
+
+            Console.WriteLine("Select car from the list: ");
+            for (byte i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {cars[i].Model}");
+            }
+            Console.Write("INPUT THE NUMBER OF THE SECOND CAR: ");
+            byte numberOfCar2 = 0;
+            bool isValidCarNumber2 = byte.TryParse(Console.ReadLine(), out numberOfCar2);
+            while (true)
+            {
+                if (numberOfCar == numberOfCar2)
+                {
+                    Console.WriteLine("That car is already racing, pleace choose another car ");
+                    isValidCarNumber2 = byte.TryParse(Console.ReadLine(), out numberOfCar2);
+                }
+                else break;
+            }
+            
+            
+            numberOfDriver -= 1;
+            numberOfCar -= 1;
+            numberOfDriver2 -= 1;
+            numberOfCar2 -= 1;
+
+
+            if (isValidCarNumber
+                && isValidDriverNumber
+                && isValidCarNumber2
+                && isValidDriverNumber2
+                && (numberOfCar >= 0 || numberOfCar <= 3)
+                 && (numberOfDriver >= 0 || numberOfDriver <= 3)
+                  && (numberOfCar2 >= 0 || numberOfCar2 <= 3)
+                   && (numberOfDriver2 >= 0 || numberOfDriver2 <= 3)
+                )
+            {
+                Car firstCar = cars[numberOfCar];
+                Car secondCar = cars[numberOfCar2];
+
+                firstCar.Driver = drivers[numberOfDriver];
+                secondCar.Driver = drivers[numberOfDriver2];
+
+                Car.RaceCars(firstCar, secondCar);
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid numbers");
+            }
+            Console.ReadLine();
         }
     }
 }
