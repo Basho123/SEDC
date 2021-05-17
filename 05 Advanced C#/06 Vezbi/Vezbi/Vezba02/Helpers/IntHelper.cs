404e8082130b60160e32dd85632e994b2e0fcf26
Ivan Jamandilovski <jamandilovski@gmail.com>
2021-05-17 20:39:47 +0200

    fixed some of the requirements

--- 05 Advanced C#/06 Vezbi/Vezbi/Vezba02/Helpers/IntHelper.cs
+++ 05 Advanced C#/06 Vezbi/Vezbi/Vezba02/Helpers/IntHelper.cs
@@ -8,6 +8,6 @@ namespace Vezba02.Helpers
     public static class IntHelper
     {
         public static bool IsEven(this int inputInt) => inputInt % 2 == 0;
-        public static int GetNFromList(this List<int> inputList) => inputList.FirstOrDefault();
+        public static List<int> GetNFromList(this List<int> inputList, int numberOfItemsToGet) => inputList.GetRange(0, numberOfItemsToGet);
     }
 }
